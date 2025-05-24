ad8b3c81dce2c162b1bb6d35b0d5ae4a42289a10
Jesse Squires <jesse@jessesquires.com>
2025-05-24 13:35:47 -0700

    update makefile and scripts

--- scripts/deploy_github.zsh
+++ scripts/deploy_github.zsh
@@ -2,8 +2,14 @@
 
 # pulls changes from github and deploys to NearlyFreeSpeech.net
 
+set -e
+
 echo '⬇️  Pulling changes from GitHub'
 git pull github master
+
+echo '⏩  install and build'
+make install
+make build
 
 echo '⏩  git status'
 git status
